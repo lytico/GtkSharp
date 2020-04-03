e5baabe77a54545f8268030b7e4151137cab855b
lytico <lytio@gmail.com>
2020-04-03 20:18:27 +0200

    Samples: add ImageDrawn

---
+++ Source/Samples/Sections/Widgets/ImageSection.cs
@@ -0,0 +1,33 @@
+﻿// This is free and unencumbered software released into the public domain.
+// Happy coding!!! - GtkSharp Team
+
+using System.Collections.Generic;
+using Gdk;
+using Gtk;
+
+namespace Samples
+{
+
+    [Section(ContentType = typeof(ImageBox), Category = Category.Widgets)]
+    class ImageSection : ListSection
+    {
+        public ImageSection()
+        {
+            AddItem(CreateContainer());
+        }
+
+        public (string, Widget) CreateContainer() {
+            Pixbuf image = default;
+            using (var stream = Util.GetResourceStream(typeof(ImageSection).Assembly,"Testpic.png")) {
+                image = new Pixbuf(stream);
+            }
+
+            var container = new ImageBox(image);
+
+
+            return ($"{nameof(ImageBox)}:", container);
+        }
+    }
+
+}
+
