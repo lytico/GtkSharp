00c14bc1d6fc2b7f9b4619cfe4757c6719d0240d
lytico <lytio@users.sourceforge.net>
2020-10-29 14:16:50 +0100

    Samples: enhance LabelSection.cs with sized Labels

--- Source/Samples/Sections/Widgets/LabelSection.cs
+++ Source/Samples/Sections/Widgets/LabelSection.cs
@@ -1,6 +1,7 @@
 ﻿// This is free and unencumbered software released into the public domain.
 // Happy coding!!! - GtkSharp Team
 
+using System.Runtime.CompilerServices;
 using Gtk;
 
 namespace Samples
@@ -11,6 +12,9 @@ namespace Samples
         public LabelSection()
         {
             AddItem(CreateSimpleLabel());
+            AddItem(CreateSizedLabel());
+            AddItem(CreateCharSizedLabel());
+            AddItem(CreateSizedBoxedLabel());
             AddItem(CreateMarkupLabel());
         }
 
@@ -27,6 +31,43 @@ namespace Samples
             return ("Label :", label);
         }
 
+        public (string, Widget) CreateSizedLabel ()
+        {
+            var label = new Label {
+                LabelProp = "This is a sized label",
+                // right align text, center is default
+                Xalign = 1f,
+                WidthRequest = 10,
+            };
+
+            return ($"Label (Size={label.WidthRequest}):", label);
+        }
+
+        public (string, Widget) CreateSizedBoxedLabel()
+            {
+            HBox box6 = new HBox ();
+            var with = 10;
+            for (int n=0; n<15; n++) {
+                var w = new Label ("TestLabel" + n) {
+                    WidthRequest = with,
+                    Expand =  false,
+                };
+                box6.PackStart (w,false,false,1);
+            }
+            return ($"Labels Boxed (Size={with}):", box6);
+        }
+        
+        public (string, Widget) CreateCharSizedLabel()
+        {
+            var label = new Label {
+                LabelProp = "This is a charsized label",
+                // left align text, center is default
+                Xalign = 0f, 
+                WidthChars = 10
+            };
+
+            return ($"Label (WidthChars={label.WidthChars}):", label);
+        }
         public (string, Widget) CreateMarkupLabel()
         {
             var label = new Label();
